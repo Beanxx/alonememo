e1a38cf82eaa0a61c3ddfeee6c18ea83504af162
beanxx <tnqls991211@naver.com>
2021-05-22 11:15:44 +0900

    플라스크 앱 생성할 때 데이터베이스 이름 지정

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from pymongo import MongoClient
 
+# mongodb 추가
 client = MongoClient('localhost', 27017)
 db = None
 
+# 플라스트 프레임워크에서 지정한 함수 이름
 # 플라스트 동작시킬 때 create_app() 함수의 결과로 리턴
 
-def create_app(database_name='sparta'):
+def create_app(database_name='sparta'):  # default 값으로 'sparta'를 쓰겠다.
     # 플라스크 웹 서버 생성하기
     app = Flask(__name__)
     app.debug = True
     app.config.from_pyfile('config.py')
 
+    # 파이썬 중급 - 전역 변수를 함수 내부에서 수정 가능하게 만들어줌
     global db
     db = client.get_database(database_name)
 
